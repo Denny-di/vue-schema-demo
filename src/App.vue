78aa92ac1f48d6aaa2a22e2ce370646552732b88
dudi <luobo.du@ctonetech.com>
2025-04-28 11:52:45 +0800

    feat: eslint9 + prettier

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+
+console.log('🌟🌟🌟🌟🌟TEO')
+
+const a = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  aa: 11,
+  bb: 12,
+  cc: 13,
+  dd: 14,
+  ee: 15,
+  ff: 16,
+  gg: 17,
+  hh: 18,
+  ii: 19,
+  jj: 20,
+  aaa: 21
+}
 </script>
 
 <template>
