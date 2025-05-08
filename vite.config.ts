f4a45f4921c7ab5d40e8fc4732eab99ed6681bb9
luobo <drenny@163.com>
2025-05-08 15:52:51 +0800

    build: 自动化部署

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import Components from 'unplugin-vue-components/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vue-schema-demo/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
