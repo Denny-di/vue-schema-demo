a0db57ace9296b66d986fcab9fadbbd6a85255e3
luobo <drenny@163.com>
2025-05-08 17:55:45 +0800

    fix: vite.config.ts 配置生产环境

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { resolve } from 'path'
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/vue-schema-demo/' : '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/vue-schema-demo/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
@@ -32,4 +32,4 @@ export default defineConfig({
       dts: 'types/auto-components.d.ts'
     })
   ]
-})
+}))
