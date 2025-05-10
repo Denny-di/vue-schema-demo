1ff7c4e831196a52f84a4f8f553a5c2049801150
luobo <drenny@163.com>
2025-05-10 16:16:39 +0800

    fix: vFormLayout 优化

--- src/components/VForm/components/VFormLayout.vue
+++ src/components/VForm/components/VFormLayout.vue
@@ -26,22 +26,24 @@ const change = (...args: any[]) => emit('change', ...args)
     <template v-for="item in schema" :key="item.prop">
       <el-col v-if="!item.hidden" class="!pr-0 !pl-0" :span="item.span ?? defaultSpan(item.type)">
         <el-tabs v-if="item.type === 'el-tabs'" :model-value="'0'" v-bind="item.props" class="flex-1 overflow-hidden">
-          <el-tab-pane v-for="tab in item.options" :key="tab.label" :label="tab.label" v-bind="tab.props">
-            <VFormLayout
-              v-if="tab.children?.length"
-              v-model:model="model"
-              :schema="tab.children"
-              :gutter="gutter"
-              :col-span="colSpan"
-              v-bind="$attrs"
-              @change="change"
-            >
-              <!-- 自定义插槽 -->
-              <template v-for="slotName in slotsName" :key="slotName" #[slotName]="slotProps">
-                <slot :name="slotName" v-bind="slotProps"></slot>
-              </template>
-            </VFormLayout>
-          </el-tab-pane>
+          <template v-for="tab in item.options" :key="tab.label">
+            <el-tab-pane v-if="!tab.hidden" :label="tab.label" v-bind="tab.props">
+              <VFormLayout
+                v-if="tab.children?.length"
+                v-model:model="model"
+                :schema="tab.children"
+                :gutter="gutter"
+                :col-span="colSpan"
+                v-bind="$attrs"
+                @change="change"
+              >
+                <!-- 自定义插槽 -->
+                <template v-for="slotName in slotsName" :key="slotName" #[slotName]="slotProps">
+                  <slot :name="slotName" v-bind="slotProps"></slot>
+                </template>
+              </VFormLayout>
+            </el-tab-pane>
+          </template>
         </el-tabs>
 
         <el-form-item v-else class="w-full mr-0" :label="item.label" :prop="item.prop" v-bind="item.itemProps">
